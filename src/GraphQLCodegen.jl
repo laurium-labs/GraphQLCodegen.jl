ba064e96844057fd53fbfa56c574f2886657793b
Jason Lehto <jason@lauriumlabs.com>
2022-12-03 22:28:19 -0500

    Remove Redundancies and Simplify

--- src/GraphQLCodegen.jl
+++ src/GraphQLCodegen.jl
@@ -1,36 +1,20 @@
 module GraphQLCodegen
 
-  function parse_nt(T::Any, s)::T
-    s
-  end
+  parse_nt(T::Any, s) = convert(T,s)
+  parse_nt(::Type{Union{T2, Nothing}}, vec::Vector) where T2 = parse_nt.(eltype(T2), vec)
+  parse_nt(::Type{Vector{T2}}, vec::Vector) where T2 = parse_nt.(T2, vec)
   
-  function parse_nt(::Type{Union{T2, Nothing}}, vec::Vector) where T2
-    [map(v -> parse_nt(eltype(T2), v), vec)...]
-  end
-
   function parse_nt(::Type{Vector{Union{T2, Nothing}}}, vec::Vector) where T2
-    [map(v -> isnothing(v) ? nothing : parse_nt(T2, v), vec )...]
+    map(v -> isnothing(v) ? nothing : parse_nt(T2, v), vec )
   end
-
-  function parse_nt(::Type{Vector{T2}}, vec::Vector) where T2
-    [map(v -> parse_nt(T2, v), vec )...]
-  end
-  
-  
+    
   function parse_nt(T::Type, d::Dict)::T
-    T((haskey(d, String(t)) ?  parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
-  end
-  
-  function parse_nt(::Type{Union{T, Nothing}}, d::Dict) where {T} 
-    T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)) )
+    T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
   end
 
   macro gql_str(str::String)
-    # str is the query to turn into a function call
-    return str
+    return str # str is the query to turn into a function call
   end
 
-
-  export parse_nt
-  export @gql_str
+  export parse_nt, @gql_str
 end # module
