cbbf8e1d59e6661ead1704a532159db699d496f7
JasonLehto <jason@lauriumlabs.com>
2022-12-26 15:09:51 -0500

    Allow a conversion to custom types

--- src/GraphQLCodegen.jl
+++ src/GraphQLCodegen.jl
@@ -1,23 +1,33 @@
 module GraphQLCodegen
 
-  parse_nt(T::Any, s) = convert(T,s)
-  parse_nt(::Type{Union{T2, Nothing}}, vec::Vector) where T2 = isempty(vec) ? [] : parse_nt.(eltype(T2), vec)
-  parse_nt(::Type{Vector{T2}}, vec::Vector) where T2 = isempty(vec) ? [] : parse_nt.(T2, vec)
-  
-  function parse_nt(::Type{Vector{Union{T2, Nothing}}}, vec::Vector) where T2
-    isempty(vec) ? [] : map(v -> isnothing(v) ? nothing : parse_nt(T2, v), vec)
+function parse_nt(T::Type, s::String)
+  if string(T) == s
+    return T
+  elseif s ∈ string.(subtypes(T))
+    return eval(Meta.parse(s))
+  else
+    return throw(ArgumentError("String $s cannot be converted to $T or any of its subtypes ($(subtypes(T)))"))
   end
-    
-  function parse_nt(::Type{Union{T, Nothing}}, d::Dict) where {T} 
-    return T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
-  end
-  function parse_nt(T::Type, d::Dict)::T
-    T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
-  end
+end
 
-  macro gql_str(str::String)
-    return str # str is the query to turn into a function call
-  end
+parse_nt(T::Any, s) = convert(T, s)
+parse_nt(::Type{Union{T2,Nothing}}, vec::Vector) where {T2} = isempty(vec) ? [] : parse_nt.(eltype(T2), vec)
+parse_nt(::Type{Vector{T2}}, vec::Vector) where {T2} = isempty(vec) ? [] : parse_nt.(T2, vec)
 
-  export parse_nt, @gql_str
+function parse_nt(::Type{Vector{Union{T2,Nothing}}}, vec::Vector) where {T2}
+  isempty(vec) ? [] : map(v -> isnothing(v) ? nothing : parse_nt(T2, v), vec)
+end
+
+function parse_nt(::Type{Union{T,Nothing}}, d::Dict) where {T}
+  return T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
+end
+function parse_nt(T::Type, d::Dict)::T
+  T((haskey(d, String(t)) ? parse_nt(fieldtype(T, t), d[String(t)]) : nothing for t in fieldnames(T)))
+end
+
+macro gql_str(str::String)
+  return str # str is the query to turn into a function call
+end
+
+export parse_nt, @gql_str
 end # module
